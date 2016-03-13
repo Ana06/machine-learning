8a06e6439fdcb223469897bf8e75b98630bf0722
Ana María Martínez Gómez <anamma06@gmail.com>
2016-03-13 19:49:14 +0100

    Fisher Linear Discriminant
    c_comp and classify_fisher methods added

--- fisher.py
+++ fisher.py
@@ -22,6 +22,46 @@ class Fisher:
         """ Attribute intitialization """
         self.w = None
         self.c = None
+        
+    def c_comp(self, p0, p1, sigma0, sigma1, m0_proy, m1_proy):
+        """ Computes the c threshold to assign a class to a (projected) point.
+            We obtain the exact roots that are local optima of the error
+            estimation and then we choose the one that minimizes it.
+
+        Parameters
+        ----------
+        p0 : float
+            prior probability for the first class
+        p1 : float
+            prior probability for the second class
+        sigma0 : float
+            maximum likelihood estimator for the std deviation of the
+            projected first class
+        sigma1 : float
+            maximum likelihood estimator for the std deviation of the
+            projected second class
+        m0_proy : float
+            maximum likelihood estimator for the mean of the projected
+            first class
+        m1_proy : float
+            maximum likelihood estimator for the mean of the projected
+            second class
+
+        """
+        a = -1 / 2 / sigma0**2 + 1 / 2 / sigma1**2
+        b = m0_proy / sigma0**2 - m1_proy / sigma1**2
+        c = -1 / 2 * m0_proy**2 / sigma0**2 + 1 / 2 * m1_proy**2 / \
+            sigma1**2 + np.log(p0 / sigma0) - np.log(p1 / sigma1)
+        pol_der = np.array([2 * a, b])
+        disc = np.sqrt(b**2 - 4 * a * c)
+        rootp = (-b + disc) / (2 * a)
+        rootm = (-b - disc) / (2 * a)
+        roots = [rootp, rootm]
+        index = np.argmin(np.polyval(pol_der, roots))
+        self.c = roots[index]
+
+        # c = average of the proyected means (alternative method)
+        # self.c = 0.5 * (m0_proy + m1_proy)
 
     def train_fisher(self, x0, x1):
         """ Trains with the sample data provided for the Fisher's Linear
@@ -49,4 +89,23 @@ class Fisher:
         x0_proy, x1_proy = self.w.T.dot(x0), self.w.T.dot(x1)
         sigma0, sigma1 = np.std(x0_proy), np.std(x1_proy)
         self.c_comp(p0, p1, sigma0, sigma1, m0_proy, m1_proy)
+        
+        def classify_fisher(self, x):
+        """ Classifies the given points thanks to the previous training.
+
+        Parameters
+        ----------
+        x : list of points from the first class, given as
+            [[x0,...,xD],...,[z0,...,zD]] where D is the number of dimensions
+
+        Returns
+        -------
+        list of integers
+            [c0,c1,...,cM] : where ci is the class of the ith point for each i
+            from 0 to M, and ci = 0 or ci = 1
+
+        """
+		x = np.asarray(x)
+		return ((x.dot(self.w) >= self.c).astype(int)).tolist()
+		
 
